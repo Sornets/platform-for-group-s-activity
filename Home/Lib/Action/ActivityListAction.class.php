2dbc70385c7616cc3652fc98a66e2818230919b6
Sornets <hellosxc@163.com>
2015-05-17 22:58:36 +0800

    build template files
    build template files, and create "activity" template and "activityList"
    template.

--- Home/Lib/Action/ActivityListAction.class.php
+++ Home/Lib/Action/ActivityListAction.class.php
@@ -2,7 +2,8 @@
 // 本类由系统自动生成，仅供测试用途
 class ActivityListAction extends Action {
     public function index($state, $time, $uid){//渲染活动列表页面
-	
+		//state:0 all, 1 future, 2 current, 3 end
+		//time :0 all, 1 today, 2 tomorrow, 3 week
 	
 	}
 
