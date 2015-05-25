223a6437ce8a2a216d2230f27e9f8514d8a5f6fb
Sornets <hellosxc@163.com>
2015-05-25 22:17:05 +0800

    finish create activity controller

--- Home/Lib/Action/ActivityListAction.class.php
+++ Home/Lib/Action/ActivityListAction.class.php
@@ -82,6 +82,55 @@ class ActivityListAction extends Action {
 	}
 
 	public function newActs(){//最新活动列表
+		//state:0/null all, 1 future, 2 current, 3 end
+		//time :0/null all, 1 today, 2 tomorrow, 3 week
+		$place_model = M('place'); //获取中文地点用
+		$user_model = M('users');  //用户昵称用
+		$act_model = M('activity');//活动信息用
+		$act_agree_model = M('agree');//活动感兴趣数用
+		$com_model = M('comment'); //获取评论、评论数用
+		$com_agree_model = M('comment_agree');//评论点赞
+		$com_disagree_model = M('comment_disagree');//评论反对
+		$config_model = M('config');//获取网站配置
+
+		//$pageTitle string
+		$pageTitle = "活动列表";
+		$this->assign('pageTitle', $pageTitle);
+
+		//$id
+		if($id){
+			$this->assign('id', $id);
+		}
+
+		//$siteName
+		$config = $config_model->find(1);
+		$site_name = $config['site_name'];
+		$this->assign('siteName', $site_name);
+
+		//$newComments
+		$newComments = $com_model->order('id desc')->limit('0, 10')->select();
+		//重构评论数据结构 rebuild comments struct
+		foreach($newComments as &$comment){
+			$user = $user_model->find($comment['uid']);
+			$comment['header'] = $user['header'];
+			$comment['nickname'] = $user['nickname'];
+			$comment['agree_num'] = count($com_agree_model->where("cid=" . $comment['id'])->select());
+			$comment['disagree_num'] = count($com_disagree_model->where("cid=" . $comment['id'])->select());
+		}
+		$this->assign('newComments', $newComments);
+
+		//$act array
+		$current_time = time();
+		import('ORG.Util.Page');// 导入分页类
+		
+		$count      = $act_model->where("start_time > $current_time")->count();// 查询满足要求的总记录数
+		$Page       = new Page($count,15);// 实例化分页类 传入总记录数和每页显示的记录数
+		$show       = $Page->show();// 分页显示输出
+		// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+		$acts = $act_model->where("start_time > $current_time")->order('id desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+		$this->assign('acts',$acts);// 赋值数据集
+		$this->assign('page',$show);// 赋值分页输出
 	
+		$this->display();
 	}
 }
--- Home/Lib/Action/CreatAction.class.php
+++ Home/Lib/Action/CreatAction.class.php
@@ -1,11 +1,68 @@
 <?php
 // 本类由系统自动生成，仅供测试用途
 class CreatAction extends Action {
+	public function _initialize(){
+		login();
+	}
     public function index(){//渲染创建活动页面
+		$config_model = M('config');
+		$place_model = M('place');
 		
+		//$pageTitle
+		$pageTitle = "创建新活动";
+		$this->assign('pageTitle', $pageTitle);
+
+		//$rule
+		$config = $config_model->find(1);
+		$rule_name = $config['rule_name'];
+		$rule_content = $config['rule_content'];
+
+		$this->assign('rule_name', $rule_name);
+		$this->assign('rule_content', $rule_content);
+		
+		//$siteName
+		$siteName = $config['site_name'];
+		$this->assign('siteName', $siteName);
+
+		//places
+		$places = $place_model->select();
+		$this->assign('places', $places);
+
+		$this->display();
 	}
 
 	public function creatActivity(){//创建活动
-	
+		$act_model = M('activity');
+		$user_model = M('users');
+
+		$uid = session('uid');
+		$act = array();
+		$act = $_POST;
+		$act['uid'] = $uid;
+		
+		//$start_time & $end_time
+		$start_time_str = $_POST['start_date'] . ' ' . $_POST['start_time'] . ':00';
+		$end_time_str = $_POST['end_date'] . ' ' . $_POST['end_time'] . ':00';
+		$act['start_time'] = strtotime($start_time_str);
+		$act['end_time'] = strtotime($end_time_str);
+
+		//$creat_time
+		$act['creat_time'] = time();
+		
+		//$state
+		$user = $user_model->find($uid);
+		if($user['level'] == 1){
+			$act['state'] = 1;
+		}
+		else{
+			$act['state'] = 0;
+		}
+
+		if($act_model->add($act)){
+			$this->success("创建活动成功");
+		}
+		else{
+			$this->error("创建活动失败");
+		}
 	}
 }
