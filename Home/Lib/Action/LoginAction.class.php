1bfddc1016fe4c9fd4696aac6dddbb49b95cceaf
Sornets <hellosxc@163.com>
2015-05-26 22:23:51 +0800

    finish user login page

--- Home/Lib/Action/LoginAction.class.php
+++ Home/Lib/Action/LoginAction.class.php
@@ -6,8 +6,18 @@ class LoginAction extends Action {
     }
 
 	public function checkLogin(){//检查用户名&密码
+		$user_model = M('users');
+		
 		$email = $_POST['email'];
 		$password = $_POST['password'];
-
+		
+		$user = $user_model->where("email='" . $email . "'")->find();
+		if($user && $user['password'] == $password){
+			session('uid', $user['id']);
+			$this->success("欢迎，" . $user['nickname'] . "！", "index.php?m=Index&a=index");
+		}
+		else{
+			$this->error("登录失败，请检查用户名或密码");
+		}
 	}
 }
